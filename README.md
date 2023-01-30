d7e583743478dbeb0aeff253a72a754f85a5661f
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-01-29 23:12:19 -0700

    adjustments for md/code

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Coding-Quiz--Challenge-4
+This is a simple coding quiz application designed to test your programming skills.
+
+## Features
+- Multiple choice questions
+- Easy to use
+- Score tracking
+- Very Responsive
+
+## How to use
+
+1. Open the application
+2.  Answer the questions 
+3. Track your score over time
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Screenshots
+![Intro of page](/img/introPage.png)
+![First question](/img/questionPage.png)
+![Results](/img/scorePage.png)
+
+
---
+++ assets/questions.js
@@ -0,0 +1,58 @@
+// create an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+        question: "what does JSON stand for?",
+        answer: " JavaScript Object Notation",
+        options: [
+            "Jelly Sandwiches Onions Nachos",
+            "JavaScript Object Notation",
+            "JavaScript Output Notation ",
+            "JavaScript Operator Number",
+            
+
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What is the scope of a variable declared with var in javascript",
+        answer: "Function Scope",
+        options: [
+            "Global Scope",
+            "Function Scope",
+            "Block Scope ",
+            "None of the above",
+            
+            
+
+        ]
+    },
+    
+    {
+        numb: 3,
+        question: "How you declare a function in JavaScript",
+        answer: "function myFunction(){}",
+        options: [
+            "var myFunction = function(){}",
+            "let thisFunction = function(){}",
+            "function myFunction(){} ",
+            "return myFunction()",
+            
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "How do you select an element by its id in JavaScript",
+        answer: "document.getElementById(id)",
+        options: [
+            "document.getElementById(id)",
+            "document.querySelector(id)",
+            "document.querySelectorAll(id) ",
+            "document.getElementById[]",
+            
+
+        ]
+    }
+]
